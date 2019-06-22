c3a86cfbf5e3d4df2d82b9ded31cefc1218a974b
SoundM <galina45192@gmail.com>
2019-06-22 20:49:56 +0200

    Делаем диалоговое окно перетаскиваемым. Исправление замечаний

--- js/dialog.js
+++ js/dialog.js
@@ -4,53 +4,60 @@
 (function () {
   var setupDialogElement = document.querySelector('.setup');
   var dialogHandler = setupDialogElement.querySelector('.upload'); // Находим  элемент, за который будем тащить наш диалог upload.
+  var dragged = false; // Устанавливаем флаг для решения конфликта между добавлением аватарки и перетаскиванием т.к.оба действия производятся при клике на один и тотже элемент
+  var startCoords = { // Начальные координаты точки курсора, с которой мы начали перемещать диалог.
+    x: 0,
+    y: 0
+  };
+  var onMouseDown = function (evtMouseDown) {
+    evtMouseDown.preventDefault();
+    startCoords = { // Координаты точки, с которой мы начали перемещать диалог.
+      x: evtMouseDown.clientX,
+      y: evtMouseDown.clientY
+    };
+    return startCoords;
+  };
 
-  dialogHandler.addEventListener('mousedown', function (evt) { // обработаем событие начала перетаскивания нашегодиалога mousedown.
-    evt.preventDefault();
+  // Функция вычисления координат от смещения
+  var onMouseMove = function (evtMouseMove) {
+    evtMouseMove.preventDefault();
 
-    var startCoords = { // Координаты точки, с которой мы начали перемещать диалог.
-      x: evt.clientX,
-      y: evt.clientY
+    var shift = {
+      x: startCoords.x - evtMouseMove.clientX,
+      y: startCoords.y - evtMouseMove.clientY
     };
 
-    var dragged = false; // Устанавливаем флаг для решения конфликта между добавлением аватарки и перетаскиванием т.к.оба действия производятся при клике на один и тотже элемент
+    startCoords = {
+      x: evtMouseMove.clientX,
+      y: evtMouseMove.clientY
+    };
 
-    // Функция вычисления координат от смещения
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      dragged = true; // выбор аватара — это нажатие без перемещения, а если мы нажали и начали двигать курсор,то действие выбора файла надо отменить
+    setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+    setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
 
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
+  };
+
+  // Функция сброса обработчика события движения и отпускания мыши
+  var onMouseUp = function (evtMouseUp) {
+    evtMouseUp.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (moveEvt) {
+        moveEvt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault);
       };
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
 
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
+  dialogHandler.addEventListener('mousedown', function (evtMouseDown) { // обработаем событие начала перетаскивания нашегодиалога mousedown.
+    evtMouseDown.preventDefault();
+    dragged = true; // выбор аватара — это нажатие без перемещения, а если мы нажали и начали двигать курсор,то действие выбора файла надо отменить
 
-      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
-      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
-
-    };
-
-    // Функция сброса обработчика события движения и отпускания мыши
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-
-      if (dragged) {
-        var onClickPreventDefault = function (moveEvt) {
-          moveEvt.preventDefault();
-          dialogHandler.removeEventListener('click', onClickPreventDefault);
-        };
-        dialogHandler.addEventListener('click', onClickPreventDefault);
-      }
-    };
-
+    document.addEventListener('mousedown', onMouseDown);
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
